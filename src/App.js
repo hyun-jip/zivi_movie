185a339a398f517edd61e52ce039351fa61bbecf
hyun-jip <frank910@naver.com>
2021-02-01 20:51:11 +0900

    zivi_movie_end

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-const charInfo = [
-  {
-    id: 1,
-    name: "Suyeon",
-    image: "https://t1.daumcdn.net/cfile/tistory/217AB7395538EF7B1C",
-  },
-
-  {
-    id: 2,
-    name: "Hyunjip",
-    image:
-      "https://lh3.googleusercontent.com/proxy/m6GI6Lnfqtczl-EoIOojaCHKmSgE2r3OdJ216hUWDBxDtYqvPOmqzBj1iqiyUUDf2tiPJtebTPwzYxGNEXtFln0cRQvWRB_6JpSMdLOZKzUpN-tAAAk-KJwy2suJewqXwOGr4tx3XFi4T69G5Q582iINnoUgIqCdStZHUsGSuVIvlvnOIPFTG_myrS5fv2_sPlB_uAL8Ss2OQFSYJpG5kmsJtq_wt0xhWpqm76-fNvPZ4d-VL9iSeQxbzsvLNWbri3iuE0JsFkE2wRvsmKPCTXGrNbUInnlBaZvUqPKH4zgIxWg",
-  },
-];
-
-function Welcome({ name, picture }) {
-  return (
-    <div>
-      <h2>Welcome {name}</h2>
-      <img src={picture} alt={name} />;
-    </div>
-  );
-}
-
-Welcome.propTypes = {
-  name: PropTypes.string.isRequired,
-};
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Zivi World!!!</h1>
-      {charInfo.map((char) => (
-        <Welcome key={char.id} name={char.name} picture={char.image} />
-      ))}
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
   );
 }
 
